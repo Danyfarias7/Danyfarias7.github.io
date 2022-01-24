d2297429e1e100b2af89545bc56651c17f000bb1
Daniel Farías <dfsoftwaredeveloper@gmail.com>
2022-01-24 17:04:34 -0300

    Al hacer click en las cards se añadio un div con el nombre don el heading de la card, esto se creo en el documento cards.js

--- js/carrito.js
+++ js/carrito.js
@@ -1,4 +1,6 @@
 
+
+// Abrir y cerrar el modal
 var elemSectionCarrito = document.getElementsByClassName('section-carrito')[0]
 
  
@@ -9,12 +11,22 @@ var btnCarrito = document.querySelector(".search-bar__carrito-container a")
 btnCarrito.addEventListener('click', ()=>{
 
 
-var nuevaCardCarrito = document.createElement('div')
-nuevaCardCarrito.innerHTML = "algo"
-
 
     elemSectionCarrito.classList.toggle('section-carrito--visible')
-    elemSectionCarrito.appendChild(nuevaCardCarrito)
+
   
 
 })
+
+
+
+// Añadir contenido al modal
+
+// var modalPLus = document.getElementsByClassName('plus-and-minus__plus')[0]
+
+
+// modalPLus.addEventListener('click', ()=>{
+
+//      console.log('hola');
+
+// })
